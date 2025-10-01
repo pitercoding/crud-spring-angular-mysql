9c7c7db763232256c4447809c5ba966bb4db963e
pitercoding <piterg.bio@gmail.com>
2025-10-01 21:27:29 +0200

    Initial commit: frontend + backend skeleton

---
+++ backend/src/main/java/com/pitergomes/CrudSpringApplication.java
@@ -0,0 +1,13 @@
+package com.pitergomes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringApplication.class, args);
+	}
+
+}
