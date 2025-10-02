974d5c70def12e7ce9f4ca2434fdf70e21eb5974
pitercoding <piterg.bio@gmail.com>
2025-10-02 21:51:32 +0200

    feat(backend): add GET endpoint to fetch all customers from database

--- backend/src/main/java/com/pitergomes/controller/CustomerController.java
+++ backend/src/main/java/com/pitergomes/controller/CustomerController.java
@@ -5,6 +5,8 @@ import com.pitergomes.service.CustomerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
@@ -16,5 +18,10 @@ public class CustomerController {
     @PostMapping("/customer")
     public Customer postCustomer(@RequestBody Customer customer){
         return customerService.postCustomer(customer);
+    }
+
+    @GetMapping("/customers")
+    private List<Customer> getAllCustomer(){
+       return customerService.getAllCustomer();
     }
 }
